2ba7b65d920063127509759426ab2741acf12154
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 19:06:48 +0300

    Create ActorServiceImpl.java

---
+++ src/main/java/com/workintech/demo/service/ActorServiceImpl.java
@@ -0,0 +1,43 @@
+package com.workintech.demo.service;
+
+
+import com.workintech.demo.entity.Actor;
+import com.workintech.demo.exceptions.ApiException;
+import com.workintech.demo.repository.ActorRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class ActorServiceImpl implements ActorService {
+
+    private final ActorRepository actorRepository;
+
+    @Override
+    public List<Actor> findAll() {
+        return actorRepository.findAll();
+    }
+
+    @Override
+    public Actor findById(long id) {
+        Optional<Actor> foundActor = actorRepository.findById(id);
+        if (foundActor.isPresent()) {
+            return foundActor.get();
+        }
+        throw new ApiException("actor is not found with id: " + id, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public Actor save(Actor actor) {
+        return actorRepository.save(actor);
+    }
+
+    @Override
+    public void delete(Actor actor) {
+        actorRepository.delete(actor);
+    }
+}
